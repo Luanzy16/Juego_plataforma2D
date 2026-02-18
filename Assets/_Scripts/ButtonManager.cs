14175c48f5af5182d43eb247248ddb2d6c9bc5b9
Luis Sánchez <luanzy1610@gmail.com>
2026-02-18 14:43:56 -0500

    Menu/UI del juego (ejemplo)

---
+++ Assets/_Scripts/ButtonManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonManager : MonoBehaviour
+{
+    public void StartGame()
+    {
+        Debug.Log("Inicio el juego");
+    }
+
+    public void Options()
+    {
+        Debug.Log("Abro las opciones");
+    }
+
+    public void Archivements()
+    {
+               Debug.Log("Abro los logros");
+    }
+
+    public void ExitGame()
+    {
+            Debug.Log("Salgo del juego");
+    }
+
+}
